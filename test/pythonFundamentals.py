faf52c7c278707cb6d1619f36f75d6ce3c775fdf
GlenCaku <glencaku@gmail.com>
2023-03-31 16:11:12 +0200

    first commit

---
+++ test/pythonFundamentals.py
@@ -0,0 +1,25 @@
+list = ['item1',24,True]
+list.append('nice')
+#print(list)
+
+person = {
+    "name" : 'Glen',
+    "last name" : 'Caku',
+    "age" : 31 ,
+    }
+#print(name)
+
+x = 10
+a = 10
+if a>x:
+    print(f'{a} is bigger than {x}')
+elif a==x : 
+    print(f'{a} is equal than {x}')
+else :
+    print(f'{a} is smaller than {x}')
+
+
+for x in 'Hello':
+    print(x)
+
+
